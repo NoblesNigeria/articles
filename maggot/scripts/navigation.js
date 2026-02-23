229866ff11de0eb1958a259d28010a002c40f509
NoblesNigeria <noblesnigeria@gmail.com>
2026-02-23 11:47:52 +0000

    good job

--- maggot/scripts/navigation.js
+++
@@ -1,7 +0,0 @@
-const hamButton = document.getElementById(`ham-btn`);
-const navMenu = document.getElementById('better');
-
-hamButton.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-    hamButton.classList.toggle('show');
-});
